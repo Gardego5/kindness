f4d7ccba9d4475f8650012a97008e546a673a842
Gardego5 <garrett.davis.7777@gmail.com>
2022-11-23 00:33:59 -0800

    projects -> index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,24 @@
-const Home = () => {
-  return null;
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Projects = () => {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/project")
+      .then((res) => (res.status === 200 ? res.json() : false))
+      .then((data) => setProjects(data));
+  }, []);
+
+  return projects?.length
+    ? projects.map(({ id, name }, idx) => (
+        <Link key={idx} href={`/${id}`}>
+          {name}
+        </Link>
+      ))
+    : projects === null
+    ? "Loading..."
+    : "You have no projects.";
 };
 
-export default Home;
+export default Projects;
