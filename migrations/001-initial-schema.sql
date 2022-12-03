81694fb70fa3bcf0ac6b487484a591a6fa961a45
Gardego5 <garrett.davis.7777@gmail.com>
2022-12-03 00:35:14 -0800

    schema updates (alerts!)

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -121,19 +121,70 @@ CREATE TABLE users_groups (
   CONSTRAINT FK_Users_Groups
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
   CONSTRAINT FK_Groups_Users
-    FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE,
+    FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE
+);
+
+CREATE TYPE alert_placement
+    AS ENUM (
+      'login',
+      'project',
+      'signup'
+    );
+
+CREATE TABLE alerts (
+  "id"                  SERIAL PRIMARY KEY,
+  "location"            alert_placement NOT NULL,
+  "content"             TEXT NOT NULL,
+  "start_date"          DATE,
+  "end_date"            DATE,
+  "times_to_display"    INTEGER
+);
+
+CREATE TABLE alerts_users (
+  "id"                  SERIAL PRIMARY KEY,
+  "alert_id"            INTEGER NOT NULL,
+  "user_id"             INTEGER NOT NULL,
+  CONSTRAINT FK_Alerts_Users
+    FOREIGN KEY (alert_id) REFERENCES alerts (id) ON DELETE CASCADE,
+  CONSTRAINT FK_Users_Alerts
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE alerts_groups (
+  "id"                  SERIAL PRIMARY KEY,
+  "alert_id"            INTEGER NOT NULL,
+  "group_id"            UUID NOT NULL,
+  CONSTRAINT FK_Alerts_Groups
+    FOREIGN KEY (alert_id) REFERENCES alerts (id) ON DELETE CASCADE,
+  CONSTRAINT FK_Groups_Alerts
+    FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE
+);
+
+CREATE TABLE alerts_projects (
+  "id"                  SERIAL PRIMARY KEY,
+  "alert_id"            INTEGER NOT NULL,
+  "project_id"          INTEGER NOT NULL,
+  CONSTRAINT FK_Alerts_Projects
+    FOREIGN KEY (alert_id) REFERENCES alerts (id) ON DELETE CASCADE,
+  CONSTRAINT FK_Projects_Alerts
+    FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE
 );
 
 -- Down
 
-DROP TABLE              users_groups;
-DROP TABLE              groups_projects;
-DROP TABLE              groups;
-DROP TABLE              users_visits;
-DROP FUNCTION           usersVisitsInTheirProjects(INTEGER, INTEGER);
-DROP TABLE              users_projects;
-DROP TABLE              visits;
-DROP FUNCTION           timeslotAndVisitProjectMatch(INTEGER, INTEGER);
-DROP TABLE              timeslots;
-DROP TABLE              projects;
-DROP TABLE              users;
+DROP TABLE IF EXISTS    alerts_projects;
+DROP TABLE IF EXISTS    alerts_groups;
+DROP TABLE IF EXISTS    alerts_users;
+DROP TABLE IF EXISTS    alerts;
+DROP TYPE IF EXISTS     alert_placement;
+DROP TABLE IF EXISTS    users_groups;
+DROP TABLE IF EXISTS    groups_projects;
+DROP TABLE IF EXISTS    groups;
+DROP TABLE IF EXISTS    users_visits;
+DROP FUNCTION IF EXISTS usersVisitsInTheirProjects(INTEGER, INTEGER);
+DROP TABLE IF EXISTS    users_projects;
+DROP TABLE IF EXISTS    visits;
+DROP FUNCTION IF EXISTS timeslotAndVisitProjectMatch(INTEGER, INTEGER);
+DROP TABLE IF EXISTS    timeslots;
+DROP TABLE IF EXISTS    projects;
+DROP TABLE IF EXISTS    users;
